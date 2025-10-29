56594fcb204fda827c4fd28e604557caf5d3823d
mostafa <mustmustapha67@gmail.com>
2025-10-29 23:55:18 +0100

    add bonus functions

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#include "libft.h"
+#include <stdlib.h>
+
+t_list  *ft_lstnew(void *content)
+{
+    t_list  *node;
+
+    node = (t_list *)malloc(sizeof(t_list));
+    if (!node)
+        return (NULL);
+    node->content = content;
+    node->next = NULL;
+    return (node);
+}
---
+++ ft_lstsize.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+
+}
+
+int main()
+{
+    
+}
